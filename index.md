d248535c407f35f8a8db1cc0555e44e07e04f24e
Vadum-Badun <vadym.kolomiiets1@gmail.com>
2026-03-05 10:57:34 -0600

    Created file for new assignment

--- index.md
+++ index.md
@@ -4,7 +4,9 @@
 - add demos here
 - [Ball Array](04-ball-array)
 - [Generative Art](gen-art)
+- 
 
 
 ## Assignments
 - [Interactive scene](interactive-scene)
+- [Object Notation and Arrays](object-arrays)
---
+++ object-arrays/sketch.js
@@ -0,0 +1,15 @@
+// Object/Array assignment
+// Vadym Kolomiiets
+// Date
+//
+// Extra for Experts:
+// 
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+}
