dae553820609a82f926a8ed67efbbffa83f27fa4
Vadum-Badun <vadym.kolomiiets1@gmail.com>
2026-03-31 11:02:58 -0600

    Started Tic Tac Toe
    Realized we have to make a game...doing it now.

--- grid-based/sketch.js
+++ grid-based/sketch.js
@@ -1,91 +1,57 @@
-// Grid based drawing
+// Grid based Tic Tac Toe
 // Vadym Kolomiiets
 // Date
 //
 // Extra for Experts:
-// Adapting Python code into JS, as well as turtle library
+//
 
 //----------------------------------------------------CREDITS--------------------------------------------------
-//
+//https://youtu.be/GTWrWM1UsnA?si=TCFLqeE1PYHtJNuw 
 //
 //--------------------------------------------------END OF CREDITS---------------------------------------------
 
-// #Allows us to continue program until user gives us proper answer
-let doing_work = true;
+let board = [
+  ['O', 'X', 'O'],
+  ['X', 'X', 'O'],
+  ['X', 'O', 'X'],
+];
 
-//Input
-let myInput;
-let submitButton;
-let userInput;
+let player_one = 'X';
+let player_two = 'O';
+
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(400, 400);
 
-  //copy from last project
-  myInput = createInput('To draw enter 1. For Gallery enter 2');
-  myInput.position(width/2 -90, height/2);
-  myInput.size(250);
-  submitButton = createButton('Submit');
-  submitButton.position(myInput.x + 100, height / 2 + 50);
-  submitButton.mousePressed(saveInput);
 }
 
 function draw() {
   background(220);
-  askForInput();
-}
+  let w = width / 3;
+  let h = height / 3;
 
-function askForInput(){
-  while(doing_work){
-    //User drawing
-    if(userInput === "1"){
-      //load the script
-    }
-    //Gallery
-    else if( userInput === "2"){
-      myInput = null;
-      myInput = createInput('Enter 1 for Jesus, 2 for Racoon');
+  for(let i = 0; i < 3; i++){
+    for(let j = 0; j < 3; j++){
+      let x = w * i + w / 2;
+      let y = h * j + h /2;
+      let spot = board[i][j];
+      textSize(32);
+      if(spot === player_two){
+        noFill();
+        ellipseMode(CORNER);
+        ellipse(x, y, w);
+      }
+      else if(spot === player_one){
+        let xsize = w / 2;
+        line(x,y,x+w,y+h);
+        line(x + w, y, x, y+h);
+      }
+      
     }
   }
 }
-function saveInput(){
-   userInput = myInput.value();
-
-  
-  //We need to know what to do
-  if(userInput === '') {
-    return;
-  }
-}
 
 
 
-// #Allows us to continue program until user gives us proper answer
-// doing_work = True
 
-// #Our program itself, in this while loop
-// while doing_work == True:
-//     #Asking our user for his choice
-//     task = screen.textinput("Task", "Would you like to draw yourself, or look at our gallery? Enter 1 for drawing, and 2 to take a look")
-//     #User drawing mode
-//     if task == "1":
-//         import vadym_kolomiiets_user_drawing_turtle
-//         doing_work = False
-//     #Gallery Mode
-//     elif task == "2":
-//         picture = screen.textinput("Picture", "Awesome! Would you like to take a look on Jesus or Racoon? Enter 1 for Jesus and 2 for Racoon ")
-//         if picture == "1":
-//             #We do import here, as for some particular reason, it breaks the order in which we want
-//             #to call function(it's being called before we ask user)
-//             import vadym_kolomiiets_jesus_grid_drawing
-//             doing_work = False
-//         elif picture == "2":
-//             import  vadym_kolomiiets_racoon_grid_drawing
-//             doing_work = False
-//         #Foolproofing, restarting proccess
-//         else:
-//             print("I don't know what you exactly want...Try again!")
-//     #Foolproofing, restarting proccess
-//     else:
-//         print("I don't know what you exactly want...Try again!")
-        
+       
