172b203f558d0bee16e8eac13032862c4b1ca958
Vadum-Badun <vadym.kolomiiets1@gmail.com>
2026-04-13 10:59:23 -0600

    More work
    Finished grid, working on player selection, game logic

--- grid-based/sketch.js
+++ grid-based/sketch.js
@@ -11,18 +11,25 @@
 //--------------------------------------------------END OF CREDITS---------------------------------------------
 
 let board = [
-  ['O', 'X', 'O'],
-  ['X', 'X', 'O'],
-  ['X', 'O', 'X'],
+  ['', '', ''],
+  ['', '', ''],
+  ['', '', ''],
 ];
 
-let player_one = 'X';
-let player_two = 'O';
+let players = ['X', 'O'];
 
+let available = [];
+
+let currentPlayer;
 
 function setup() {
   createCanvas(400, 400);
-
+  currentPlayer = random(players);
+  for(let j = 0; j < 3; j++){
+    for(let i = 0; i < 3; i++){
+      available.push([i,j]);
+    }
+  }
 }
 
 function draw() {
@@ -30,25 +37,37 @@ function draw() {
   let w = width / 3;
   let h = height / 3;
 
-  for(let i = 0; i < 3; i++){
-    for(let j = 0; j < 3; j++){
-      let x = w * i + w / 2;
-      let y = h * j + h /2;
+  //setting up the grid
+  line(w, 0 , w, height);
+  line(w*2, 0 , w*2, height);
+  line(0, h , width, h);
+  line(0, h * 2, width, h*2);
+
+  //make it as a separate function, so lame
+  for(let j = 0; j < 3; j++){
+    for(let i = 0; i < 3; i++){
+      let x = w * j + w/2;
+      let y = h * i + h/2;
       let spot = board[i][j];
       textSize(32);
-      if(spot === player_two){
+      strokeWeight(4);
+      if(spot === players[1]){
         noFill();
-        ellipseMode(CORNER);
-        ellipse(x, y, w);
+        ellipse(x, y, w/2);
       }
-      else if(spot === player_one){
-        let xsize = w / 2;
-        line(x,y,x+w,y+h);
-        line(x + w, y, x, y+h);
+      else if(spot === players[0]){
+        let xsize = w / 4;
+        line(x-xsize, y-xsize, x + xsize, y + xsize);
+        line(x + xsize, y-xsize, x-xsize, y + xsize);
       }
       
     }
   }
+}
+
+function nextTurn(){
+  let index = floor(random(available.length));
+  let spot = available.splice(index,1);
 }
 
 
